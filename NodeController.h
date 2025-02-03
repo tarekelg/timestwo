02791de85a73e121419b29450d1553d5019a7bc2
tarekelg <162974882+tarekelg@users.noreply.github.com>
2025-02-03 16:34:40 +0100

    Update NodeController.h

--- NodeController.h
+++ NodeController.h
@@ -1,20 +1,46 @@
 #pragma once
 #include <boost/lockfree/queue.hpp>
-#include <atomic>
-#include <thread>
 
+//Klasse für Operationen bzgl. FIFO1 und FIFO2
 
-class NodeController {
+class QueueController {
 
 public:
 	
+	//Kontruktor reserviert speicher für FIFOs
+	QueueController();
 	
-	NodeController(void);
-	void run();
+	//Sendet neuen Wert an FIFO1
+	bool sendToFIFO1(int );
+
+	//Sendet neuen Wert an FIFO^2
+	bool sendToFIFO2(int );
+
+	//Liest Wert aus FIFO1
+	bool readFromFIFO1(int&);
+
+	//Liest Wert aus FIFO2
+	bool readFromFIFO2(int&);
+
+	//Prüft ob FIFO1 leer ist
+	bool checkIfFIFOEmpty1(void);
+
+	//Prüft ob FIFO2 leer ist
+	bool checkIfFIFOEmpty2(void);
+
+	
+
+	//Destruktor leert die FIFOs
+	~QueueController(void);
+
 
 private:
+	//boost::lockfree::queue Objecte werden Abgekapselt und so vor unbefugten Zugriff geschützt
+	std::unique_ptr<boost::lockfree::queue<int>> ptrF1;
+	std::unique_ptr<boost::lockfree::queue<int>> ptrF2;
 
-	std::unique_ptr <std::thread> t1Ptr;
-	std::unique_ptr <std::thread> t2Ptr;
+	
+
+
 
 };
