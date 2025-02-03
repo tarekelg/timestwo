3cec4748156f3652f52423bb1155276d3d86f520
tarekelg <162974882+tarekelg@users.noreply.github.com>
2025-02-03 16:37:57 +0100

    Update QueueController.h

--- QueueController.h
+++ QueueController.h
@@ -1,74 +1,46 @@
-#include "QueueController.h"
-#include <iostream>
+#pragma once
+#include <boost/lockfree/queue.hpp>
 
+//Klasse für Operationen bzgl. FIFO1 und FIFO2
 
-// Konstruktor
-QueueController::QueueController()
-	: ptrF1(std::make_unique<boost::lockfree::queue<int>>(100)),
-	ptrF2(std::make_unique<boost::lockfree::queue<int>>(100)) { 
+class QueueController {
+
+public:
+	
+	//Kontruktor reserviert speicher für FIFOs
+	QueueController();
+	
+	//Sendet neuen Wert an FIFO1
+	bool sendToFIFO1(int );
+
+	//Sendet neuen Wert an FIFO^2
+	bool sendToFIFO2(int );
+
+	//Liest Wert aus FIFO1
+	bool readFromFIFO1(int&);
+
+	//Liest Wert aus FIFO2
+	bool readFromFIFO2(int&);
+
+	//Prüft ob FIFO1 leer ist
+	bool checkIfFIFOEmpty1(void);
+
+	//Prüft ob FIFO2 leer ist
+	bool checkIfFIFOEmpty2(void);
 
 	
-}
 
-//Sendet neuen Wert an FIFO1
-bool QueueController::sendToFIFO1(int input) {
-	if (!ptrF1) {
-		std::cerr << "Error: ptrF1 is null in readFromFIFO1!" << std::endl;
-		return false;
-	}
-	return ptrF1->push(input);
+	//Destruktor leert die FIFOs
+	~QueueController(void);
+
+
+private:
+	//boost::lockfree::queue Objecte werden Abgekapselt und so vor unbefugten Zugriff geschützt
+	std::unique_ptr<boost::lockfree::queue<int>> ptrF1;
+	std::unique_ptr<boost::lockfree::queue<int>> ptrF2;
+
+	
+
+
+
 };
-
-//Sendet neuen Wert an FIFO^2
-bool QueueController::sendToFIFO2(int input) {
-	if (!ptrF2) {
-		std::cerr << "Error: ptrF1 is null in readFromFIFO1!" << std::endl;
-		return false;
-	}
-	return ptrF2->push(input);
-};
-
-//Liest Wert aus FIFO1
-bool QueueController::readFromFIFO1(int& value) {
-	if (!ptrF1) {
-		std::cerr << "Error: ptrF1 is null in readFromFIFO1!" << std::endl;
-		return false;
-	}
-	return ptrF1->pop(value);
-}
-
-//Liest Wert aus FIFO2
-bool QueueController::readFromFIFO2(int& value) {
-	if (!ptrF2) {
-		std::cerr << "Error: ptrF1 is null in readFromFIFO1!" << std::endl;
-		return false;
-	}
-	return ptrF2->pop(value);
-}
-
-//Prüft ob FIFO1 leer ist
-bool QueueController::checkIfFIFOEmpty1() {
-	if (!ptrF1) {
-		std::cerr << "Error: ptrF1 is null in readFromFIFO1!" << std::endl;
-		return false;
-	}
-	return ptrF1->empty();
-}
-
-//Prüft ob FIFO2 leer ist
-bool QueueController::checkIfFIFOEmpty2() {
-	return ptrF2->empty();
-}
-
-// Destructor
-QueueController::~QueueController() {
-	int value;
-
-	// FIFO1 leeren
-	while (readFromFIFO1(value))
-		;
-
-	// FIFO2 leeren
-	while (readFromFIFO2(value))
-		;
-}
